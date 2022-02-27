893b16f106dd37f0cda0b0dcee211c9ad2937d45
Memo Chou <memochou1993@gmail.com>
2022-02-28 01:44:14 +0800

    Add interact script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,8 @@
 async function main() {
-  // 實例化一個 HelloWorld 合約工廠
   const HelloWorld = await ethers.getContractFactory('HelloWorld');
 
-  // 使用合約工廠的 deploy 方法來進行部署
   const hello_world = await HelloWorld.deploy('Hello World!');
   
-  // 印出合約地址
   console.log('Contract deployed to address:', hello_world.address);
 };
 
---
+++ scripts/interact.js
@@ -0,0 +1,23 @@
+const { API_KEY, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
+
+const contract = require('../artifacts/contracts/HelloWorld.sol/HelloWorld.json');
+
+const alchemyProvider = new ethers.providers.AlchemyProvider(network='rinkeby', API_KEY);
+
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+  const message = await helloWorldContract.message();
+  console.log('The message is:', message);
+
+  console.log('Updating the message...');
+  const tx = await helloWorldContract.update('This is the new message.');
+  await tx.wait();
+
+  const newMessage = await helloWorldContract.message();
+  console.log('The new message is:', newMessage); 
+};
+
+main();
